20b49f211e9eff92716c8885f6712157fa36eb69
Nikita Karapuzov <karapuzov.nikita@gmail.com>
2020-05-24 18:34:40 +0700

    add i18n

---
+++ src/i18n/index.ts
@@ -0,0 +1,36 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+import locales from "src/locales";
+
+const detectorOptions = {
+  caches: ["localStorage", "cookie"],
+  lookupCookie: "i18next",
+  lookupLocalStorage: "i18nextLng",
+  lookupQuerystring: "lng",
+  order: [
+    "localStorage",
+    "navigator",
+    "cookie",
+    "htmlTag",
+    "querystring",
+    "path",
+  ],
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    defaultNS: "common",
+    detection: detectorOptions,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: locales,
+  });
+
+export default i18n;
