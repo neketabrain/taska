b3720b1933f721de5ea174a33024f9d8459e06c9
Nikita Karapuzov <karapuzov.nikita@gmail.com>
2020-05-30 02:11:01 +0700

    update locales

--- src/locales/ru/index.ts
+++ src/locales/ru/index.ts
@@ -1,6 +1,5 @@
 import common from "./common.json";
 import errors from "./errors.json";
-import header from "./header.json";
 import login from "./login.json";
 import registration from "./registration.json";
 import reset from "./reset.json";
@@ -8,7 +7,6 @@ import reset from "./reset.json";
 export default {
   common,
   errors,
-  header,
   reset,
   registration,
   login,
