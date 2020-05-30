30953a5f9e8ef518033837e7c9961167f521cb44
Nikita Karapuzov <karapuzov.nikita@gmail.com>
2020-05-30 20:13:54 +0700

    upload avatar

--- src/components/molecules/ProfileCard/ProfileCard.tsx
+++ src/components/molecules/ProfileCard/ProfileCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+import { UploadAvatar } from "src/components";
 import { ApplicationStore } from "src/store";
 
 import {
@@ -9,25 +10,30 @@ import {
   UserName,
   UserEmail,
   Picture,
+  MockAvatar,
 } from "./ProfileCard.styles";
 import { ProfileCardProps } from "./ProfileCard.types";
 
 function ProfileCard(props: ProfileCardProps): JSX.Element {
   const { className } = props;
 
-  const { user } = useSelector((state: ApplicationStore) => state);
+  const user = useSelector((state: ApplicationStore) => state.user);
 
   const userName = user?.displayName || "";
   const userEmail = user?.email || "";
   const userAvatar = user?.photoURL;
 
+  const [firstName, lastName] = userName.split(" ");
+  const avatarText = firstName[0] + lastName[0];
+
   return (
     <Container className={className}>
-      {!!userAvatar && (
-        <Picture>
-          <UserAvatar src={userAvatar} alt="User avatar" />
-        </Picture>
-      )}
+      <Picture>
+        <UploadAvatar />
+
+        {!!userAvatar && <UserAvatar src={userAvatar} alt="User avatar" />}
+        {!userAvatar && <MockAvatar>{avatarText}</MockAvatar>}
+      </Picture>
 
       <UserName title={userName}>{userName}</UserName>
       <UserEmail title={userEmail}>{userEmail}</UserEmail>
