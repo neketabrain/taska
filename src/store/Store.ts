3dd30c0409908bf3777a8a4161046cc059a0da61
Nikita Karapuzov <karapuzov.nikita@gmail.com>
2020-05-30 18:02:53 +0700

    add redux-thunk

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,5 +1,6 @@
-import { Store, combineReducers, createStore } from "redux";
+import { Store, combineReducers, createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
 import { ApplicationStore, RootReducer } from "./types";
 import { UserReducer } from "./user";
@@ -8,10 +9,11 @@ const rootReducer = combineReducers<RootReducer>({
   user: UserReducer,
 });
 
-function configureStore(
-  initialState?: ApplicationStore
-): Store<ApplicationStore> {
-  return createStore(rootReducer, initialState, composeWithDevTools());
+function configureStore(initialState?: ApplicationStore): Store {
+  const enhancers = applyMiddleware(thunk);
+  const composedEnhancers = composeWithDevTools(enhancers);
+
+  return createStore(rootReducer, initialState, composedEnhancers);
 }
 
 export default configureStore;
