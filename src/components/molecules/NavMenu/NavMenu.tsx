557a6ca88bef6a551897afd5ecd5499aec80ac88
Nikita Karapuzov <karapuzov.nikita@gmail.com>
2020-06-07 00:27:08 +0700

    update nav menu

--- src/components/molecules/NavMenu/NavMenu.tsx
+++ src/components/molecules/NavMenu/NavMenu.tsx
@@ -2,27 +2,29 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 import { Icons } from "src/components";
+import { NavItemProps } from "src/components/atoms/NavItem/NavItem.types";
 import { ROUTES } from "src/constants";
 
 import { Container, NavItemList, Divider, MenuItem } from "./NavMenu.styles";
 import { NavMenuProps } from "./NavMenu.types";
 
-const navItems = [
+const navItems: NavItemProps[] = [
   {
     label: "newTask",
-    path: ROUTES.NEW_TASK,
+    to: ROUTES.NEW_TASK,
     exact: true,
     Icon: Icons.FilledAdd,
   },
   {
     label: "taskList",
-    path: ROUTES.TASKS,
-    exact: true,
+    to: ROUTES.TASKS,
     Icon: Icons.FilledList,
+    isActive: (_, { pathname }): boolean =>
+      /tasks/.test(pathname) && !/new/.test(pathname),
   },
   {
     label: "settings",
-    path: ROUTES.SETTINGS,
+    to: ROUTES.SETTINGS,
     Icon: Icons.FilledSettings,
   },
 ];
@@ -37,7 +39,7 @@ function NavMenu(props: NavMenuProps): JSX.Element {
       <NavItemList>
         {navItems.map(({ label, ...navItem }) => (
           <MenuItem
-            key={navItem.path}
+            key={navItem.to}
             label={t(`navMenu.${label}`)}
             {...navItem}
           />
@@ -46,7 +48,7 @@ function NavMenu(props: NavMenuProps): JSX.Element {
         <Divider />
         <MenuItem
           label={t("navMenu.logout")}
-          path={ROUTES.LOGOUT}
+          to={ROUTES.LOGOUT}
           Icon={Icons.FilledExit}
           exact={true}
         />
